4a11e49812980fd51d8168224280914a040a0a06
sokolovstanislav <ignateva-1946@mail.ru>
2022-12-22 19:19:43 +0300

    Lesson21: CRUD, REST, DAO (Data Access Object)

---
+++ src/main/java/ru/sokolov/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package ru.sokolov.dao;
+
+import org.springframework.stereotype.Component;
+import ru.sokolov.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Pavel"));
+        people.add(new Person(++PEOPLE_COUNT, "Anna"));
+        people.add(new Person(++PEOPLE_COUNT, "Darya"));
+        people.add(new Person(++PEOPLE_COUNT, "Sofiya"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
