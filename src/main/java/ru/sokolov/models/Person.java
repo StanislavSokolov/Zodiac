2f03f0ad85a1b75e7aca55b325d608fdc09eb638
sokolovstanislav <ignateva-1946@mail.ru>
2022-12-27 14:22:51 +0300

    Lesson23.CRUD_App3.

--- src/main/java/ru/sokolov/models/Person.java
+++ src/main/java/ru/sokolov/models/Person.java
@@ -8,6 +8,11 @@ public class Person {
 
     }
 
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
@@ -21,11 +26,6 @@ public class Person {
     }
 
     public void setName(String name) {
-        this.name = name;
-    }
-
-    public Person(int id, String name) {
-        this.id = id;
         this.name = name;
     }
 }
