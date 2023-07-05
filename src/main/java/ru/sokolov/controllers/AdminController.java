bc246979c7069eff2c558a4f42e3bbf6f1a6c99f
sokolovstanislav <ignateva-1946@mail.ru>
2023-07-05 18:44:53 +0300

    Lesson43. Select->option.

---
+++ src/main/java/ru/sokolov/controllers/AdminController.java
@@ -0,0 +1,31 @@
+package ru.sokolov.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.sokolov.dao.PersonDAO;
+import ru.sokolov.models.Person;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private  final PersonDAO personDAO;
+
+    public AdminController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String adminPage(Model model, @ModelAttribute("person") Person person) {
+        model.addAttribute("people", personDAO.index());
+
+        return "people/adminPage";
+    }
+
+    @PatchMapping("/add")
+    public String makeAdmin(@ModelAttribute("person") Person person) {
+        System.out.println(person.getId());
+
+        return "redirect:/people";
+    }
+}
