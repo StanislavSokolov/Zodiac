c47bf61e3de7316a4a2229f44555accf4695fc8e
sokolovstanislav <ignateva-1946@mail.ru>
2023-08-08 18:12:11 +0300

    Lesson58. Spring+Hibernate. Start config.

--- src/main/java/ru/sokolov/config/SpringConfig.java
+++ src/main/java/ru/sokolov/config/SpringConfig.java
@@ -9,6 +9,10 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -19,11 +23,14 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.sql.DataSource;
 import java.util.Objects;
+import java.util.Properties;
 
 @Configuration
 @ComponentScan("ru.sokolov")
+@PropertySource("classpath:hibernate.properties")
+@EnableTransactionManagement
 @EnableWebMvc
-@PropertySource("classpath:database.properties")
+//@PropertySource("classpath:database.properties")
 public class SpringConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
@@ -70,16 +77,44 @@ public class SpringConfig implements WebMvcConfigurer {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("driver_value")));
-        dataSource.setUrl(environment.getProperty("url_value"));
-        dataSource.setUsername(environment.getProperty("username_value"));
-        dataSource.setPassword(environment.getProperty("password_value"));
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("hibernate.driver_class")));
+        dataSource.setUrl(environment.getProperty("hibernate.connection.url"));
+        dataSource.setUsername(environment.getProperty("hibernate.connection.username"));
+        dataSource.setPassword(environment.getProperty("hibernate.connection.password"));
 
         return dataSource;
     }
 
-    @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate(dataSource());
+//    @Bean
+//    public JdbcTemplate jdbcTemplate() {
+//        return new JdbcTemplate(dataSource());
+//    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+
+        return properties;
     }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("ru.sokolov.models");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+
+        return transactionManager;
+    }
+
+
 }
--- src/main/java/ru/sokolov/dao/PersonDAO.java
+++ src/main/java/ru/sokolov/dao/PersonDAO.java
@@ -15,88 +15,109 @@ import java.util.Optional;
 
 @Component
 public class PersonDAO {
-    private final JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    public PersonDAO(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
 
     public List<Person> index() {
-        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
-    }
-
-    public Optional<Person> show(String email) {
-        return jdbcTemplate.query("SELECT * FROM Person WHERE email=?", new Object[] {email},
-                new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
+        return null;
     }
 
     public Person show(int id) {
-        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
-                .stream().findAny().orElse(null);
+        return null;
     }
 
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person(name, age, email, address) VALUES(?, ?, ?, ?)", person.getName(), person.getAge(),
-                person.getEmail(), person.getAddress());
+
     }
 
     public void update(int id, Person updatedPerson) {
-        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=?, address=? WHERE id=?", updatedPerson.getName(),
-                updatedPerson.getAge(), updatedPerson.getEmail(), updatedPerson.getAddress(), id);
+
     }
 
     public void delete(int id) {
-        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+
     }
 
-    ///////
-    ///////
-    ///////
+//    private final JdbcTemplate jdbcTemplate;
+//
+//    @Autowired
+//    public PersonDAO(JdbcTemplate jdbcTemplate) {
+//        this.jdbcTemplate = jdbcTemplate;
+//    }
 
-    public void testMultipleUpdate() {
-        List<Person> people = create1000People();
-
-        long before = System.currentTimeMillis();
-
-        for (Person person: people) {
-            jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES(?, ?, ?)", person.getName(), person.getAge(),
-                    person.getEmail());
-        }
-
-        long after = System.currentTimeMillis();
-
-        System.out.println("Time: " + (after - before));
-    }
-
-    public void testBatchUpdate() {
-        List<Person> people = create1000People();
-
-        long before = System.currentTimeMillis();
-
-        jdbcTemplate.batchUpdate("INSERT INTO Person(name, age, email) VALUES(?, ?, ?)", new BatchPreparedStatementSetter() {
-            @Override
-            public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
-//                preparedStatement.setInt(1, people.get(i).getId());
-                preparedStatement.setString(1, people.get(i).getName());
-                preparedStatement.setInt(2, people.get(i).getAge());
-                preparedStatement.setString(3, people.get(i).getEmail());
-            }
-
-            @Override
-            public int getBatchSize() {
-                return people.size();
-            }
-        });
-
-        long after = System.currentTimeMillis();
-
-        System.out.println("Time: " + (after - before));
-    }
-
-    private List<Person> create1000People() {
-        List<Person> people = new ArrayList<>();
-        for (int i = 0; i < 1000; i++) people.add(new Person(i, "Name" + i, 32, "test" + i + "@mail.ru", "some address"));
-        return people;
-    }
+//    public List<Person> index() {
+//        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
+//    }
+//
+//    public Optional<Person> show(String email) {
+//        return jdbcTemplate.query("SELECT * FROM Person WHERE email=?", new Object[] {email},
+//                new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
+//    }
+//
+//    public Person show(int id) {
+//        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+//                .stream().findAny().orElse(null);
+//    }
+//
+//    public void save(Person person) {
+//        jdbcTemplate.update("INSERT INTO Person(name, age, email, address) VALUES(?, ?, ?, ?)", person.getName(), person.getAge(),
+//                person.getEmail(), person.getAddress());
+//    }
+//
+//    public void update(int id, Person updatedPerson) {
+//        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=?, address=? WHERE id=?", updatedPerson.getName(),
+//                updatedPerson.getAge(), updatedPerson.getEmail(), updatedPerson.getAddress(), id);
+//    }
+//
+//    public void delete(int id) {
+//        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+//    }
+//
+//    ///////
+//    ///////
+//    ///////
+//
+//    public void testMultipleUpdate() {
+//        List<Person> people = create1000People();
+//
+//        long before = System.currentTimeMillis();
+//
+//        for (Person person: people) {
+//            jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES(?, ?, ?)", person.getName(), person.getAge(),
+//                    person.getEmail());
+//        }
+//
+//        long after = System.currentTimeMillis();
+//
+//        System.out.println("Time: " + (after - before));
+//    }
+//
+//    public void testBatchUpdate() {
+//        List<Person> people = create1000People();
+//
+//        long before = System.currentTimeMillis();
+//
+//        jdbcTemplate.batchUpdate("INSERT INTO Person(name, age, email) VALUES(?, ?, ?)", new BatchPreparedStatementSetter() {
+//            @Override
+//            public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+////                preparedStatement.setInt(1, people.get(i).getId());
+//                preparedStatement.setString(1, people.get(i).getName());
+//                preparedStatement.setInt(2, people.get(i).getAge());
+//                preparedStatement.setString(3, people.get(i).getEmail());
+//            }
+//
+//            @Override
+//            public int getBatchSize() {
+//                return people.size();
+//            }
+//        });
+//
+//        long after = System.currentTimeMillis();
+//
+//        System.out.println("Time: " + (after - before));
+//    }
+//
+//    private List<Person> create1000People() {
+//        List<Person> people = new ArrayList<>();
+//        for (int i = 0; i < 1000; i++) people.add(new Person(i, "Name" + i, 32, "test" + i + "@mail.ru", "some address"));
+//        return people;
+//    }
 }
